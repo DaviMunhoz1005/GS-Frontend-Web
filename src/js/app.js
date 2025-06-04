36ebc00d8bf5c4c34de9a95e1a32422d959036b1
Davi Munhoz <davimunhoz1005@gmail.com>
2025-06-04 17:16:21 -0300

    feat(identification-section): create a indentificator section with a observer

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,28 @@
 import { initCarousel } from './carousel.js';
+import { initSectionObserver } from './observer-section.js';
 
 initCarousel('#section-our-goals');
 initCarousel('#section-everyday-solution');
+
+initSectionObserver((visibleSectionId) => {
+    const indicator = document.getElementById('current-section');
+    if (indicator) {
+        indicator.textContent = visibleSectionId.replace('section-', '').replaceAll('-', ' ');
+    }
+});
+
+const select = document.getElementById("menu-select");
+
+select.addEventListener("change", function () {
+    const value = this.value;
+    if (!value) return;
+    if (value.startsWith("#")) {
+        const section = document.querySelector(value);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+    } else {
+        window.location.href = value;
+    }
+    this.selectedIndex = 0;
+});
